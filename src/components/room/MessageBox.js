247e14fc1bcaa4288a2e2cb4bb355e30da2740d9
Jason <jas0nle@yahoo.com>
2019-11-12 17:37:41 -0600

    Added scroll to bottom to chat

--- src/components/room/MessageBox.js
+++ src/components/room/MessageBox.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import MessageItem from './MessageItem';
 
 class MessageBox extends Component {
-    state = {  }
+    constructor(){
+        super();
+        this.state = {
+
+        }
+        this.chatRef = React.createRef();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.messages.length !== prevProps.messages) {
+
+        }
+    }
+
+    scrollToBottom = () => {
+        this.chatRef.current.scrollTop = this.chatRef.current.scrollHeight;
+    }
 
     displayMessages = () => {
         return this.props.messages.map((message, index) => <MessageItem message={message} key={index}/>)
@@ -10,7 +26,7 @@ class MessageBox extends Component {
 
     render() { 
         return (
-            <div className="chat-box">
+            <div className="chat-box" ref={this.chatRef}>
                 {this.displayMessages()}
             </div>
         );
