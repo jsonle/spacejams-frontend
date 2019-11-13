1137c2a531c87f86d0491b429cbcbc4e75620b0f
Jason <jas0nle@yahoo.com>
2019-11-13 09:02:58 -0600

    Added playlist description to room cards

--- src/components/browse/RoomCard.js
+++ src/components/browse/RoomCard.js
@@ -4,14 +4,14 @@ import Button from 'react-bootstrap/Button';
 
 
 const RoomCard = ({ room, handleClick }) => {
-    const {id, playlist_id, playlist_image_url, playlist_name, users} = room
+    const {id, playlist_id, playlist_image_url, playlist_name, playlist_description, users} = room
     return (
         <Card>
             <Card.Img variant="top" src={playlist_image_url} />
             <Card.Body>
                 <Card.Title>{playlist_name}</Card.Title>
                 <Card.Text>
-                    Placeholder description
+                    {playlist_description}
                 </Card.Text>
                     <Button variant="warning" onClick={handleClick} value={id} name={playlist_id}>Enter Room</Button>
             </Card.Body>
